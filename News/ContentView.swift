5705e1bcee85fc881e7b6604fc16b9db0eeb9a9c
Ravi Theja <bsravi.tez@gmail.com>
2020-03-16 23:37:32 +0530

    Initial Commit

---
+++ News/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  News
+//
+//  Created by Ravi Theja on 16/03/20.
+//  Copyright © 2020 Idlebrains. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
